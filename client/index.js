0ed4ec86ac92b7844ec987e318a257cfe6a21745
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-04-06 23:39:20 +0100

    Initial attempt to parameterise drawing

--- client/index.js
+++ client/index.js
@@ -40,20 +40,75 @@ const COLOUR_TO_PEG = {
   [C.WH]: P.WH,
 };
 
-const BOARD_WIDTH = 414;
-const BOARD_HEIGHT = 736;
-const LARGE_PEG_RADIUS = 15;
-const SMALL_PEG_RADIUS = 7;
-const SMALL_PEG_HOLE_RADIUS = 6;
-const LARGE_PEG_HOLE_RADIUS = 12;
-const SECRET_ROW_CENTRE_Y = 38;
-const FIRST_ROW_CENTRE_Y = 140;
-const ROW_GAP_Y = 60;
-const FIRST_LARGE_PEG_X = 155;
-const LARGE_PEG_GAP_X = 62;
-const SMALL_PEG_GAP_Y = 10;
-const SMALL_PEG_LEFT_X = 60;
-const SMALL_PEG_RIGHT_X = 86;
+// document.body.clientWidth
+// document.body.clientHeight
+// Moto G (4): 360 x 512
+const BOARD_WIDTH = 360;
+const BOARD_HEIGHT = 512;
+// const BOARD_WIDTH = 414;
+// const BOARD_HEIGHT = 736;
+
+const GUTTER_TOP = 10;
+const GUTTER_BOTTOM = 10;
+const GUTTER_LEFT = 55;
+const GUTTER_RIGHT = 35;
+
+const SECRET_PANEL_WIDTH = 200;
+const SECRET_PANEL_HEIGHT = 45;
+const SECRET_PANEL_X = BOARD_WIDTH - SECRET_PANEL_WIDTH - GUTTER_RIGHT;
+const SECRET_PANEL_Y = GUTTER_TOP;
+const SECRET_PANEL_LIP = 10;
+const SECRET_PANEL_BORDER = 6;
+const SECRET_PANEL_HALF_BORDER = SECRET_PANEL_BORDER / 2;
+
+// We are only using stroke to get a rounding effect
+const MAIN_PANEL_STROKE = 6;
+const MAIN_PANEL_X = GUTTER_LEFT;
+const MAIN_PANEL_Y = SECRET_PANEL_Y + SECRET_PANEL_HEIGHT + 10; // TODO: hard-coded value
+const MAIN_PANEL_WIDTH = BOARD_WIDTH - GUTTER_LEFT - GUTTER_RIGHT;
+const MAIN_PANEL_HEIGHT = BOARD_HEIGHT - MAIN_PANEL_Y - GUTTER_BOTTOM;
+
+const SMALL_CUTOUT_X = MAIN_PANEL_X;
+const SMALL_CUTOUT_WIDTH = 50;
+const SMALL_CUTOUT_HALF_HEIGHT = 20;
+
+const LARGE_HOLE_CUTOUT_RADIUS = 21;
+
+const LARGE_PEG_RADIUS = 12;
+const SMALL_PEG_RADIUS = 5;
+
+const SMALL_PEG_HOLE_RADIUS = 4;
+const SMALL_PEG_HOLE_STROKE = 1;
+
+const LARGE_PEG_HOLE_RADIUS = 10;
+const LARGE_PEG_HOLE_STROKE = 4;
+const SECRET_ROW_CENTRE_Y = SECRET_PANEL_Y + SECRET_PANEL_HEIGHT / 2;
+const FIRST_ROW_CENTRE_Y = 87;
+const ROW_GAP_Y = 43;
+const FIRST_LARGE_PEG_X = SECRET_PANEL_X + 2 * LARGE_PEG_RADIUS;
+const LARGE_PEG_GAP_X = 50;
+const SMALL_PEG_GAP_Y = 7;
+const SMALL_PEG_LEFT_X = MAIN_PANEL_X + 12; // TODO: hard-coded value
+const SMALL_PEG_RIGHT_X = SMALL_PEG_LEFT_X + 26; // TODO: hard-coded value
+
+const COLOUR_MENU_BORDER = 3;
+const COLOUR_MENU_HALF_BORDER = COLOUR_MENU_BORDER / 2;
+
+const COLOUR_MENU_OUTER_X = GUTTER_LEFT;
+const COLOUR_MENU_OUTER_Y = SECRET_PANEL_Y;
+const COLOUR_MENU_OUTER_WIDTH = MAIN_PANEL_WIDTH;
+const COLOUR_MENU_OUTER_HEIGHT = SECRET_PANEL_HEIGHT;
+
+const COLOUR_MENU_INNER_X = COLOUR_MENU_OUTER_X + COLOUR_MENU_BORDER;
+const COLOUR_MENU_INNER_Y = COLOUR_MENU_OUTER_Y + COLOUR_MENU_BORDER;
+const COLOUR_MENU_INNER_WIDTH = COLOUR_MENU_OUTER_WIDTH - 2 * COLOUR_MENU_BORDER;
+const COLOUR_MENU_INNER_HEIGHT = COLOUR_MENU_OUTER_HEIGHT - 2 * COLOUR_MENU_BORDER;
+
+const POINTER_WIDTH = 20;
+const POINTER_HALF_WIDTH = POINTER_WIDTH / 2;
+const POINTER_HEIGHT = 14;
+const POINTER_TIP_X = FIRST_LARGE_PEG_X;
+const POINTER_TIP_Y = COLOUR_MENU_OUTER_Y + COLOUR_MENU_OUTER_HEIGHT + COLOUR_MENU_HALF_BORDER + POINTER_HEIGHT;
 
 const S = {
   INITIALISED: Symbol('initialised'),
@@ -71,6 +126,8 @@ const state = {
 };
 
 const board = document.getElementById("board");
+board.setAttribute("width", BOARD_WIDTH);
+board.setAttribute("height", BOARD_HEIGHT);
 const btnNewGame = document.getElementById("btnNewGame");
 const btnEnter = document.getElementById("btnEnter");
 
@@ -97,9 +154,20 @@ const createSVGElement = elementName =>
   document.createElementNS('http://www.w3.org/2000/svg', elementName);
 
 const addSecretPanel = () => {
+
+  const pathData = `
+    M${SECRET_PANEL_X + SECRET_PANEL_LIP},${SECRET_PANEL_Y}
+    h${-SECRET_PANEL_LIP}
+    v${SECRET_PANEL_HEIGHT}
+    h${SECRET_PANEL_WIDTH}
+    v${-SECRET_PANEL_HEIGHT}
+    h${-SECRET_PANEL_LIP}
+  `;
+
   const path = createSVGElement("path");
+  path.setAttribute("d", pathData);
+  path.setAttribute("stroke-width", SECRET_PANEL_BORDER);
   path.setAttribute("class", "secret-panel");
-  path.setAttribute("d", "M134 10 L124 10 L124 80 L372 80 L372 10 L362 10");
   board.appendChild(path);
   range(4).forEach(addSecretPanelLargePegHole);
 };
@@ -109,24 +177,32 @@ const addSecretPanelLargePegHole = n =>
 
 const showSecretPanelCover = () => {
 
+  const x = SECRET_PANEL_X + SECRET_PANEL_HALF_BORDER;
+  const y = SECRET_PANEL_Y + SECRET_PANEL_HALF_BORDER;
+  const w = SECRET_PANEL_WIDTH - SECRET_PANEL_BORDER;
+  const h = SECRET_PANEL_HEIGHT - SECRET_PANEL_BORDER;
+  const MARGIN = SECRET_PANEL_BORDER * 3;
+
+  const pathData = `M${x},${y} v${h} h${w} v${-h} z`;
+
   const group = createSVGElement("g");
   group.setAttribute("data-piece", "secret-panel-cover");
 
   const path = createSVGElement("path");
+  path.setAttribute("d", pathData);
   path.setAttribute("class", "secret-panel-cover");
-  path.setAttribute("d", "M127 13 L127 77 L369 77 L369 13 Z");
   group.appendChild(path);
 
   const text1 = createSVGElement("text");
-  text1.setAttribute("x", 127 + 20);
-  text1.setAttribute("y", 46);
+  text1.setAttribute("x", x + MARGIN);
+  text1.setAttribute("y", y + h / 2);
   text1.setAttribute("class", "secret-panel-cover-text-1");
   text1.appendChild(document.createTextNode("MASTER"));
   group.appendChild(text1);
 
   const text2 = createSVGElement("text");
-  text2.setAttribute("x", 369 - 20);
-  text2.setAttribute("y", 74);
+  text2.setAttribute("x", x + w - MARGIN);
+  text2.setAttribute("y", y + h - SECRET_PANEL_HALF_BORDER);
   text2.setAttribute("class", "secret-panel-cover-text-2");
   text2.appendChild(document.createTextNode("MIND"));
   group.appendChild(text2);
@@ -167,10 +243,11 @@ const addRowSmallPegHole = (row, n) => {
   const cx = n % 2 === 0 ? SMALL_PEG_LEFT_X : SMALL_PEG_RIGHT_X;
   const cy = FIRST_ROW_CENTRE_Y + (row * ROW_GAP_Y) + SMALL_PEG_GAP_Y * (n >= 2 ? +1 : -1);
   const circle = createSVGElement("circle");
-  circle.setAttribute("class", "small-peg-hole");
   circle.setAttribute("cx", cx);
   circle.setAttribute("cy", cy);
   circle.setAttribute("r", SMALL_PEG_HOLE_RADIUS);
+  circle.setAttribute("stroke-width", SMALL_PEG_HOLE_STROKE);
+  circle.setAttribute("class", "small-peg-hole");
   board.appendChild(circle);
 };
 
@@ -198,44 +275,65 @@ const addLargePegHole = (row, n, cy) => {
   const inverseRowNumber = 9 - row;
   const cx = FIRST_LARGE_PEG_X + (n * LARGE_PEG_GAP_X);
   const circle = createSVGElement("circle");
-  circle.setAttribute("class", "large-peg-hole");
   circle.setAttribute("cx", cx);
   circle.setAttribute("cy", cy);
   circle.setAttribute("r", LARGE_PEG_HOLE_RADIUS);
+  circle.setAttribute("stroke-width", LARGE_PEG_HOLE_STROKE);
+  circle.setAttribute("class", "large-peg-hole");
   circle.addEventListener("click", makeLargePegClickHandler(inverseRowNumber, n));
   board.appendChild(circle);
 };
 
 const buildPathDataForSmallHolesCutOut = row => {
   const cy = FIRST_ROW_CENTRE_Y + (row * ROW_GAP_Y);
-  const y1 = cy - 26;
-  const y2 = cy + 26;
-  const d = `M40 ${y1} L107 ${y1} L107 ${y2} L40 ${y2} Z`;
+  const y = cy - SMALL_CUTOUT_HALF_HEIGHT;
+  const d = `
+    M${SMALL_CUTOUT_X},${y}
+    h${SMALL_CUTOUT_WIDTH}
+    v${(2 * SMALL_CUTOUT_HALF_HEIGHT)}
+    h${-SMALL_CUTOUT_WIDTH}
+    z
+  `;
   return d;
 };
 
 const buildPathDataForLargeHolesCutOut = row => {
-  const dx = 21;
-  const dy = 17;
+
+  const r = LARGE_HOLE_CUTOUT_RADIUS;
+  const theta = 0.6805212246672144; // about 39 degrees
+  const dx = r * Math.cos(theta);
+  const dy = r * Math.sin(theta);
+
   const cy = FIRST_ROW_CENTRE_Y + (row * ROW_GAP_Y);
-  const y1 = cy - dy;
-  const y2 = cy + dy;
-  const r = 27;
-  const cx1 = FIRST_LARGE_PEG_X;
-  const cx2 = FIRST_LARGE_PEG_X + LARGE_PEG_GAP_X;
-  const cx3 = FIRST_LARGE_PEG_X + (2 * LARGE_PEG_GAP_X);
-  const cx4 = FIRST_LARGE_PEG_X + (3 * LARGE_PEG_GAP_X);
-  const d1 = `M${cx1 + dx} ${y2} A ${r} ${r} 0 1 1 ${cx1 + dx} ${y1}`;
-  const d2 = `L${cx2 - dx} ${y1} A ${r} ${r} 1 0 1 ${cx2 + dx} ${y1}`;
-  const d3 = `L${cx3 - dx} ${y1} A ${r} ${r} 1 0 1 ${cx3 + dx} ${y1}`;
-  const d4 = `L${cx4 - dx} ${y1} A ${r} ${r} 1 1 1 ${cx4 - dx} ${y2}`;
-  const d5 = `L${cx3 + dx} ${y2} A ${r} ${r} 1 0 1 ${cx3 - dx} ${y2}`;
-  const d6 = `L${cx2 + dx} ${y2} A ${r} ${r} 1 0 1 ${cx2 - dx} ${y2} Z`;
-  return [d1, d2, d3, d4, d5, d6].join(" ");
+  const arcWidth = 2 * dx;
+  const arcHeight = 2 * dy;
+  const interArcGap = LARGE_PEG_GAP_X - 2 * dx;
+
+  return `
+    M${FIRST_LARGE_PEG_X + dx},${cy + dy}
+    a ${r} ${r} 0 1 1 0,${-arcHeight}
+    h${interArcGap}
+    a ${r} ${r} 1 0 1 ${arcWidth},0
+    h${interArcGap}
+    a ${r} ${r} 1 0 1 ${arcWidth},0
+    h${interArcGap}
+    a ${r} ${r} 1 1 1 0,${arcHeight}
+    h${-interArcGap}
+    a ${r} ${r} 1 0 1 ${-arcWidth},0
+    h${-interArcGap}
+    a ${r} ${r} 1 0 1 ${-arcWidth},0
+    z
+  `;
 };
 
 const addMainPanel = () => {
-  const pathDataForMainPanelOutline = "M30 100 L374 100 L374 720 L30 720 Z";
+  const pathDataForMainPanelOutline = `
+    M${MAIN_PANEL_X},${MAIN_PANEL_Y}
+    h${MAIN_PANEL_WIDTH}
+    v${MAIN_PANEL_HEIGHT}
+    h${-MAIN_PANEL_WIDTH}
+    z
+  `;
   const pathDataForSmallHoleCutOuts = range(10).map(buildPathDataForSmallHolesCutOut);
   const pathDataForLargeHoleCutOuts = range(10).map(buildPathDataForLargeHolesCutOut);
   const pathData = [
@@ -244,8 +342,9 @@ const addMainPanel = () => {
     ...pathDataForLargeHoleCutOuts
   ].join(" ");
   const path = createSVGElement("path");
-  path.setAttribute("class", "main-panel");
   path.setAttribute("d", pathData);
+  path.setAttribute("stroke-width", MAIN_PANEL_STROKE);
+  path.setAttribute("class", "main-panel");
   board.appendChild(path);
 };
 
@@ -257,11 +356,11 @@ const addSmallPeg = (row, n, colour) => {
     (inverseRowNumber * ROW_GAP_Y) +
     SMALL_PEG_GAP_Y * (n >= 2 ? +1 : -1);
   const circle = createSVGElement("circle");
-  circle.setAttribute("class", "small-peg");
   circle.setAttribute("cx", cx);
   circle.setAttribute("cy", cy);
   circle.setAttribute("r", SMALL_PEG_RADIUS);
   circle.setAttribute("fill", colour);
+  circle.setAttribute("class", "small-peg");
   circle.setAttribute("data-piece", "small-peg");
   board.appendChild(circle);
 };
@@ -274,11 +373,11 @@ const addLargePeg = (row, n, colour) => {
     : FIRST_ROW_CENTRE_Y + (inverseRowNumber * ROW_GAP_Y);
   const cx = FIRST_LARGE_PEG_X + (n * LARGE_PEG_GAP_X);
   const circle = createSVGElement("circle");
-  circle.setAttribute("class", "large-peg");
   circle.setAttribute("cx", cx);
   circle.setAttribute("cy", cy);
   circle.setAttribute("r", LARGE_PEG_RADIUS);
   circle.setAttribute("fill", colour);
+  circle.setAttribute("class", "large-peg");
   circle.setAttribute("data-piece", "large-peg");
   circle.setAttribute("data-position", position);
   circle.addEventListener("click", makeLargePegClickHandler(row, n));
@@ -319,11 +418,6 @@ const showEnterButton = () => btnEnter.style.display = "block";
 const hideEnterButton = () => btnEnter.style.display = "none";
 const enableEnterButton = () => btnEnter.disabled = false;
 const disableEnterButton = () => btnEnter.disabled = true;
-
-hideEnterButton();
-addSecretPanel();
-addRows();
-addMainPanel();
 
 const onNewGame = () => {
   removePegs();
@@ -378,43 +472,52 @@ const makeColourSwatchClickHandler = colour => () => {
 const createColourMenu = () => {
 
   const outerRect = createSVGElement("rect");
-  outerRect.setAttribute("x", 30);
-  outerRect.setAttribute("y", 10);
-  outerRect.setAttribute("width", 344);
-  outerRect.setAttribute("height", 70);
+  outerRect.setAttribute("x", COLOUR_MENU_OUTER_X);
+  outerRect.setAttribute("y", COLOUR_MENU_OUTER_Y);
+  outerRect.setAttribute("rx", 5);
+  outerRect.setAttribute("ry", 5);
+  outerRect.setAttribute("width", COLOUR_MENU_OUTER_WIDTH);
+  outerRect.setAttribute("height", COLOUR_MENU_OUTER_HEIGHT);
+  outerRect.setAttribute("stroke-width", COLOUR_MENU_BORDER);
   outerRect.setAttribute("class", "colour-menu");
 
   const innerRect = createSVGElement("rect");
-  innerRect.setAttribute("x", 36);
-  innerRect.setAttribute("y", 16);
-  innerRect.setAttribute("width", 368 - 36);
-  innerRect.setAttribute("height", 74 - 16);
+  innerRect.setAttribute("x", COLOUR_MENU_INNER_X);
+  innerRect.setAttribute("y", COLOUR_MENU_INNER_Y);
+  innerRect.setAttribute("rx", 5);
+  innerRect.setAttribute("ry", 5);
+  innerRect.setAttribute("width", COLOUR_MENU_INNER_WIDTH);
+  innerRect.setAttribute("height", COLOUR_MENU_INNER_HEIGHT);
   innerRect.setAttribute("class", "colour-menu-inner");
 
   const colourMenu = createSVGElement("g");
   colourMenu.appendChild(outerRect);
   colourMenu.appendChild(innerRect);
 
-  const gap = (368 - 36) / 6;
-  const hgap = gap / 2;
+  const gap = COLOUR_MENU_INNER_WIDTH / COLOURS.length;
+  const halfGap = gap / 2;
   COLOURS.forEach((colour, index) => {
-    const cx = 36 + hgap + gap * index;
-    const cy = (16 + 74) / 2;
+    const cx = COLOUR_MENU_INNER_X + halfGap + gap * index;
+    const cy = COLOUR_MENU_INNER_Y + COLOUR_MENU_INNER_HEIGHT / 2;
     const colourSwatch = createSVGElement("circle");
-    colourSwatch.setAttribute("class", "large-peg");
     colourSwatch.setAttribute("cx", cx);
     colourSwatch.setAttribute("cy", cy);
     colourSwatch.setAttribute("r", LARGE_PEG_RADIUS);
     colourSwatch.setAttribute("fill", colour);
+    colourSwatch.setAttribute("class", "large-peg");
     colourSwatch.addEventListener("click", makeColourSwatchClickHandler(colour));
     colourMenu.appendChild(colourSwatch);
   });
 
-  const centreX = FIRST_LARGE_PEG_X;
+  const pathData = `
+    M${POINTER_TIP_X},${POINTER_TIP_Y}
+    l${-POINTER_HALF_WIDTH},${-POINTER_HEIGHT}
+    h${POINTER_WIDTH}
+    z
+  `;
   const pointer = createSVGElement("path");
-  const pathData = `M${centreX} 97 L${centreX - 10} 83 L${centreX + 10} 83 Z`;
   pointer.setAttribute("d", pathData);
-  pointer.setAttribute("class", "colou-menu-pointer");
+  pointer.setAttribute("class", "colour-menu-pointer");
 
   return { colourMenu, pointer };
 };
@@ -459,3 +562,8 @@ const toggleColourMenuFor = n =>
 
 btnNewGame.addEventListener("click", onNewGame);
 btnEnter.addEventListener("click", onEnter);
+
+hideEnterButton();
+addSecretPanel();
+addRows();
+addMainPanel();
