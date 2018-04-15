a50d4096dcdef4a11a768f43d1437a5032ca5498
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-04-15 09:35:25 +0100

    Add polyfill for element.remove()

--- client/index.js
+++ client/index.js
@@ -696,3 +696,21 @@ hideEnterButton();
 addSecretPanel();
 addRows();
 addMainPanel();
+
+// https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty("remove")) {
+      return;
+    }
+    Object.defineProperty(item, "remove", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        if (this.parentNode !== null)
+          this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
